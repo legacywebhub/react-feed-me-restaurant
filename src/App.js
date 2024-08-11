84d985f579222c4e494927158a3dcc4804995f34
Muhammad Sanni <elmuhammad72@gmail.com>
2024-08-11 15:43:41 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import Home from './Pages/Home/Home';
-import './Assets/CSS/fonts.css';
-import './App.css';
+import "./Assets/CSS/fonts.css";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import Meal from "./Pages/Meal/Meal";
+import Search from "./Pages/Search/Search";
+import ErrorPage from "./Pages/ErrorPage/ErrorPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/meal/:id" element={<Meal />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
