28f2d694280b5c815dfe0579046988083df57067
Paulson Bosah <paulsonbosah@gmail.com>
2024-08-17 14:52:44 +0100

    Update home page III

--- src/Components/Meal/Meal.js
+++ src/Components/Meal/Meal.js
@@ -5,12 +5,12 @@ import MealImg from '../../Assets/Images/meal_img.jpg'
 const Meal = ({mealName, mealType}) => {
   return (
     <div className='meal'>
-      <div className='meal__info'>
-      <div className='meal__type'>{mealType}</div>
-        <div className='meal__name'>{mealName}</div>
-      </div>
       <div className='meal__img'>
         <img src={MealImg} alt="meal" />
+      </div>
+      <div className='meal__info'>
+        <div className='meal__type'>{mealType}</div>
+        <div className='meal__name'>{mealName}</div>
       </div>
     </div>
   )
